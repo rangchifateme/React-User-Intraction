ce52d6c58fae04275b54a2e97a8a903707a4811c
Fateme <rangchi.fateme@gmail.com>
2023-05-22 11:44:25 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AddUser from './Components/Users/AddUser.js';
+function App() {
+  return (
+    <div>
+      <AddUser />
+    </div>
+  );
+}
+
+export default App;
